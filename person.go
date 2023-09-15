bcbfbe5cf8b79b1f89f14c3a86570caf82a6bda7
thormiwa <thormiwa04@gmail.com>
2023-09-15 10:46:02 +0100

    person.go

--- person.go
+++ person.go
@@ -1,6 +1,25 @@
 package main
 
-type Person struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitializeDatabase(db *sql.DB) error {
+	createTableSQL := `
+	CREATE TABLE IF NOT EXISTS person (
+		id INTEGER PRIMARY KEY,
+		name TEXT
+	);`
+
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatalf("Error creating 'person' table: %v", err)
+		return err
+	}
+
+	log.Println("Database and table 'person' initialized successfully.")
+	return nil
 }
